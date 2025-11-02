cd1efae35533d21519b4f67216847f33e79429ea
Istheaksayem <istheakahmed9345@gmail.com>
2025-11-02 21:57:05 +0600

    smart deals project started

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className='text-4xl text-center text-red-700'>Smarts deals clients</h1>
+
+      <button className="btn btn-soft btn-primary">Primary</button>
+
+    </>
+  )
+}
+
+export default App
