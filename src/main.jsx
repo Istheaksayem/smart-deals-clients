e20da6ee519f5d81fdd337f94218315b502c9632
Istheaksayem <istheakahmed9345@gmail.com>
2025-11-03 12:52:11 +0600

    Set up AuthContext , Provider and auth functions

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,36 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from './Layout/RootLayout.jsx';
+import Home from './comlponent/Home/Home.jsx';
+import AllProducts from './comlponent/AllProducts/AllProducts.jsx';
+import AuthProvider from './Context/AuthProvider.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:RootLayout,
+    children:[
+      {
+        index:true,
+        Component:Home,
+      },
+      {
+        path:'allProducts',
+        Component:AllProducts
+      }
+    ]
+   
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />,
+    </AuthProvider>
   </StrictMode>,
 )
