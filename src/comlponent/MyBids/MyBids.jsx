49627185f462f7cbf0c067185081050b26685c26
Istheaksayem <istheakahmed9345@gmail.com>
2025-11-05 23:13:40 +0600

    added a myBit section

--- src/comlponent/MyBids/MyBids.jsx
+++ src/comlponent/MyBids/MyBids.jsx
@@ -1,5 +1,6 @@
 import React, { use, useEffect, useState } from 'react';
 import { AuthContext } from '../../Context/AuthContext';
+import Swal from 'sweetalert2';
 
 const MyBids = () => {
     const { user } = use(AuthContext)
@@ -15,6 +16,43 @@ const MyBids = () => {
         }
     }, [user?.email])
 
+    const handleDeleteBid = (_id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                console.log('now delete')
+
+                fetch(`http://localhost:3000/bids/${_id}`,{
+                    method:'DELETE',
+
+                })
+                .then(res =>res.json())
+                .then(data =>{
+                  if(data.deletedCount){
+                          Swal.fire({
+                    title: "Deleted!",
+                    text: "Your bids has been deleted.",
+                    icon: "success"
+                });
+
+                const remainingBids = bids?.filter(bid =>bid._id !==_id)
+                setBids(remainingBids)
+                  }
+                })
+
+          
+            }
+        });
+
+    }
+
     return (
         <div>
             <h3>MyBids: {bids?.length}</h3>
@@ -23,46 +61,53 @@ const MyBids = () => {
                     {/* head */}
                     <thead>
                         <tr>
-                        
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                            <th></th>
+                            <th>#</th>
+                            <th>Product</th>
+                            <th>Seller</th>
+                            <th>Bid Price</th>
+                            <th>Status</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
                         {/* row 4 */}
-                    {
-                        bids.map((bid,index) => <tr key={bid._id}>
-                            <th>{index + 1} </th>
-                            <td>
-                                <div className="flex items-center gap-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle h-12 w-12">
-                                            <img
-                                                src="https://img.daisyui.com/images/profile/demo/5@94.webp"
-                                                alt="Avatar Tailwind CSS Component" />
+                        {
+                            bids?.map((bid, index) => <tr key={bid._id}>
+                                <th>{index + 1} </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle h-12 w-12">
+                                                <img
+                                                    src="https://img.daisyui.com/images/profile/demo/5@94.webp"
+                                                    alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{bid.buyer_name}</div>
+
                                         </div>
                                     </div>
-                                    <div>
-                                        <div className="font-bold">Yancy Tear</div>
-                                        <div className="text-sm opacity-50">Brazil</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                Wyman-Ledner
-                                <br />
-                                <span className="badge badge-ghost badge-sm">Community Outreach Specialist</span>
-                            </td>
-                            <td>Indigo</td>
-                            <th>
-                                <button className="btn btn-ghost btn-xs">details</button>
-                            </th>
-                        </tr>)
-                    }
+                                </td>
+                                <td>
+                                    Wyman-Ledner
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">Community Outreach Specialist</span>
+                                </td>
+                                <td>{bid.bid_price}</td>
+                                <td>
+                                    <div className="badge badge-warning">{bid.status}</div>
+                                </td>
+
+                                <th>
+                                    <button
+                                        onClick={() => handleDeleteBid(bid._id)}
+                                        className="btn btn-outline btn-xs">Remove Bid</button>
+                                </th>
+                            </tr>)
+                        }
                     </tbody>
-                
+
                 </table>
             </div>
         </div>
